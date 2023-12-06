1005000ad0892fde9e6474ada4cf479d2c4e47ec
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2023-12-06 16:10:35 +0300

    Move Planet selection into different component

---
+++ src/components/PlanetTypeList.svelte
@@ -0,0 +1,59 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  import Button from '../components/Button.svelte';
+  import { PlanetSize } from '../constants/planetSize';
+  import { PlanetTypes } from '../constants/planetTypes';
+
+  const dispatch = createEventDispatcher();
+
+  const planetTypes = Object.values(PlanetTypes);
+
+  const availableSizes = [
+    { text: "XS", size: PlanetSize.tiny },
+    { text: "S", size: PlanetSize.small },
+    { text: "M", size: PlanetSize.medium },
+    { text: "L", size: PlanetSize.large },
+    { text: "XL", size: PlanetSize.huge }
+  ];
+</script>
+
+<div class="list">
+  {#each planetTypes as type}
+    <div class="option">
+      <div class="title">{type}</div>
+
+      <div class="sizes">
+        {#each availableSizes as { text, size }}
+          <Button
+            text={text}
+            on:click={() => dispatch('add', { type, size })}
+          />
+        {/each}
+      </div>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .list{
+    display: grid;
+    grid-template-columns: repeat(2, max-content);
+    grid-gap: 5px 10px;
+  }
+
+  .option {
+    width: 100%;
+    display: contents;
+    align-items: center;
+  }
+
+  .sizes {
+    display: flex;
+    gap: 5px;
+  }
+
+  .title {
+    align-self: center;
+  }
+
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,16 @@
 <script>
   import PlanetCard from '../components/PlanetCard.svelte';
-  import Button from '../components/Button.svelte';
-  import { PlanetSize } from '../constants/planetSize';
-  import { PLANET_STATS } from '../constants/planetStats';
+  import PlanetTypeList from '../components/PlanetTypeList.svelte';
 
   let lastPlanetId = 0;
 
   let comparedPlanets = [];
 
-  const addPlanet = (planetType, size) => {
+  const handleAddPlanet = (event) => {
+    const { type, size } = event.detail;
     comparedPlanets = [
       ...comparedPlanets,
-      { id: lastPlanetId++, type: planetType, size }
+      { id: lastPlanetId++, type, size }
     ];
   }
 
@@ -19,38 +18,12 @@
     comparedPlanets = comparedPlanets.filter(planet => planet.id !== id);
   }
 
-  const planetOptions = Object.entries(PLANET_STATS)
-    .map(([type, stats]) => ({ type, stats }))
-
-  const availableSizes = [
-    { text: "XS", size: PlanetSize.tiny },
-    { text: "S", size: PlanetSize.small },
-    { text: "M", size: PlanetSize.medium },
-    { text: "L", size: PlanetSize.large },
-    { text: "XL", size: PlanetSize.huge }
-  ];
-
 </script>
 
 <div class="container">
   <aside class="planet-types">
     <h3>Planet types</h3>
-    <div class="list">
-      {#each planetOptions as planet}
-        <div
-          key={planet.name}
-          class="option"
-        >
-          <div class="title">{planet.type}</div>
-
-          <div class="sizes">
-            {#each availableSizes as { text, size }}
-              <Button text={text} on:click={() => addPlanet(planet.type, size)}/>
-            {/each}
-          </div>
-        </div>
-      {/each}
-    </div>
+    <PlanetTypeList on:add={handleAddPlanet} />
   </aside>
   <main>
     <h3>Planet comparison</h3>
@@ -85,25 +58,5 @@
     flex: 1;
   }
 
-  .planet-types .list{
-    display: grid;
-    grid-template-columns: repeat(2, max-content);
-    grid-gap: 5px 10px;
-  }
-
-  .planet-types .option {
-    width: 100%;
-    display: contents;
-    align-items: center;
-  }
-
-  .planet-types .option .sizes {
-    display: flex;
-    gap: 5px;
-  }
-
-  .planet-types .option .title {
-    align-self: center;
-  }
 
 </style>
